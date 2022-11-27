9331c07987f3cda31d2fe4bd0b6f7eda289a2230
voidrlm <varjun609@gmail.com>
2022-11-28 00:00:18 +0530

    Home page design additions

--- src/views/home.vue
+++ src/views/home.vue
@@ -16,6 +16,33 @@
       >{{ getGreetingData }},
       {{ $store.getters.currentUser.name }} !</v-card-title
     >
+
+    <v-layout justify-start class="mt-7">
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1525138079-9424be9df411?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
+        ></v-img>
+      </v-card>
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1523224949444-170258978eef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80"
+        ></v-img>
+      </v-card>
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1551818633-579bccc6210c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
+        ></v-img>
+      </v-card>
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1478296046035-d4fc77bec472?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        ></v-img>
+      </v-card>
+    </v-layout>
   </v-container>
 </template>
 <script>

c8c6ddecbe030e1c9e05ca040387522571cd8c5a
voidrlm <varjun609@gmail.com>
2022-12-21 01:11:26 +0530

    Updated posts database

---
+++ src/resources/postsDatabase.js
@@ -0,0 +1,23 @@
+export const postsData = [
+  { userId: 1, postId: 12, text: "lmao bro.", likes: 46 },
+  {
+    userId: 3,
+    postId: 12,
+    text: "SOMEONE BIG IS GETTING MARGINED CALLED ON TESLA RIGHT NOW $TSLA",
+    img: "https://s.marketwatch.com/public/resources/images/MW-HY051_tesla__ZG_20200109072602.jpg",
+    likes: 1,
+  },
+  {
+    userId: 4,
+    postId: 12,
+    text: "Getting ready for a week of programming and then two weeks of vacation!.",
+    likes: 2,
+  },
+  {
+    userId: 6,
+    postId: 12,
+    text: "Heroes get remembered, but legends never die 🐐.",
+    img: "https://pbs.twimg.com/media/Fka-ui2XkAI7c9u?format=jpg&name=small",
+    likes: 212,
+  },
+];

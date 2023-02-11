81453121bd73409eb8f5517bed46efb65bc8d61b
voidrlm <varjun609@gmail.com>
2023-02-11 19:41:30 +0530

    Added new component for stories

---
+++ src/components/stories/storiesPreview.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout justify-start class="mt-3">
+    <v-slide-group class="pa-0" show-arrows>
+      <v-slide-item
+        v-for="(story, index) in storiesData"
+        :key="index"
+        class="rounded-xl"
+      >
+        <v-card class="ma-2" height="200" width="100">
+          <v-img height="200" :src="story.img"></v-img>
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
+  </v-layout>
+</template>
+
+<script>
+import { storiesData } from "@/resources/storiesDatabase";
+export default {
+  components: {},
+  data: () => ({ storiesData }),
+  methods: {},
+};
+</script>
+<style scoped></style>

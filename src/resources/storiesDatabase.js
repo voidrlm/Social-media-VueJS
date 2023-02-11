369919f430ea5c45d63a6f3f8297cfeca1837c9b
voidrlm <varjun609@gmail.com>
2023-02-11 22:52:03 +0530

    Changes in stories data base

--- src/resources/storiesDatabase.js
+++ src/resources/storiesDatabase.js
@@ -2,7 +2,6 @@ export const storiesData = [
   {
     userId: 3,
     storyId: 6,
-    text: "Yum",
     img: "https://i.pinimg.com/originals/03/3c/db/033cdb01d3239e7342a1b3a0a16c4ab3.jpg",
     likes: 1,
     isLiked: false,
@@ -12,7 +11,6 @@ export const storiesData = [
   {
     userId: 6,
     storyId: 5,
-    text: "Woo",
     img: "https://i.pinimg.com/originals/b0/aa/19/b0aa19178f045aa01f175c7c689712b9.jpg",
     likes: 2,
     isLiked: false,
@@ -21,7 +19,6 @@ export const storiesData = [
   {
     userId: 7,
     storyId: 4,
-    text: "Night out.....",
     img: "https://i.pinimg.com/originals/06/3d/1c/063d1ca50d0892555690fa43ba3a7d37.jpg",
     likes: 3,
     isLiked: false,
@@ -31,7 +28,6 @@ export const storiesData = [
   {
     userId: 3,
     storyId: 3,
-    text: "Heyya.",
     img: "https://media.gettyimages.com/id/769713977/photo/young-woman-taking-a-selfie-in-her-bathroom-mirror.jpg?s=612x612&w=gi&k=20&c=wUVVX8tJwwEckB8aE00n5OAdDcJCKjdpxg6duupaPSA=",
     likes: 12,
     isLiked: false,
@@ -40,7 +36,6 @@ export const storiesData = [
   {
     userId: 1,
     storyId: 2,
-    text: "",
     img: "https://thumbs.dreamstime.com/b/palms-tropical-beach-white-sand-summer-vacation-travel-holiday-background-concept-caribbean-paradise-beach-palms-136606345.jpg",
     likes: 4,
     isLiked: false,
@@ -49,7 +44,6 @@ export const storiesData = [
   {
     userId: 2,
     storyId: 1,
-    text: "Zzzz.",
     img: "https://i.pinimg.com/originals/72/d7/e4/72d7e4857245fa2bd1c1a38fc3e0ab1c.jpg",
     likes: 12,
     isLiked: false,

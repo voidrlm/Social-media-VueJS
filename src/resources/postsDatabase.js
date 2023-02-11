25ac62fca8ce6f612bcca7e3e9f7e763e69d4c7f
voidrlm <varjun609@gmail.com>
2023-02-11 15:09:37 +0530

    Changes in posts db

--- src/resources/postsDatabase.js
+++ src/resources/postsDatabase.js
@@ -1,17 +1,19 @@
 export const postsData = [
-  { userId: 1, postId: 12, text: "lmao bro.", likes: 46 },
+  { userId: 1, postId: 12, text: "lmao bro.", likes: 46, isLiked: false },
   {
     userId: 3,
     postId: 12,
     text: "SOMEONE BIG IS GETTING MARGINED CALLED ON TESLA RIGHT NOW $TSLA",
     img: "https://s.marketwatch.com/public/resources/images/MW-HY051_tesla__ZG_20200109072602.jpg",
     likes: 1,
+    isLiked: false,
   },
   {
     userId: 4,
     postId: 12,
     text: "Getting ready for a week of programming and then two weeks of vacation!.",
     likes: 2,
+    isLiked: false,
   },
   {
     userId: 6,
@@ -19,5 +21,6 @@ export const postsData = [
     text: "Heroes get remembered, but legends never die 🐐.",
     img: "https://pbs.twimg.com/media/Fka-ui2XkAI7c9u?format=jpg&name=small",
     likes: 212,
+    isLiked: false,
   },
 ];

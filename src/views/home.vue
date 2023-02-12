2730d1e95018bb654f276dbdf52a1b6d2c97ec97
voidrlm <varjun609@gmail.com>
2023-02-12 22:40:20 +0530

    Usage of new post component

--- src/views/home.vue
+++ src/views/home.vue
@@ -17,68 +17,11 @@
       {{ $store.getters.currentUser.name }} !</v-card-title
     >
     <storiesGroup />
-    <v-card
-      class="mx-auto mt-5 rounded-xl"
-      elevation="0"
-      color="secondary "
-      v-for="(post, index) in postsData"
-      :key="index"
-    >
-      <v-card-title>
-        <v-list-item class="grow">
-          <v-list-item-avatar color="accent">
-            <v-img
-              class="elevation-6"
-              :alt="post.uploadedBy"
-              :src="post.uploaderAvatar"
-            ></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title
-              >{{ post.uploadedBy
-              }}<v-icon
-                size="15"
-                class="ml-2"
-                :class="post.isUploaderOnline ? 'green--text' : ''"
-                v-text="'mdi-circle'"
-              ></v-icon
-            ></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card-title>
-
-      <v-card-text class="text-h5 mx-4 mt-n2 font-weight-bold">
-        {{ post.text }}
-      </v-card-text>
-      <v-img
-        v-if="post.img"
-        class="elevation-0"
-        :alt="post.uploadedBy"
-        :src="post.img"
-        max-height="500"
-        contain
-      ></v-img>
-      <v-card-actions>
-        <v-list-item class="grow">
-          <v-row align="center" justify="end">
-            <v-btn icon @click="post.isLiked = !post.isLiked"
-              ><v-icon :color="post.isLiked ? 'pink' : ''">
-                mdi-heart
-              </v-icon></v-btn
-            >
-            <span class="subheading mr-2">{{
-              post.isLiked ? post.likes + 1 : post.likes
-            }}</span>
-            <span class="mr-1">·</span>
-            <v-icon class="mr-1"> mdi-share-variant </v-icon>
-          </v-row>
-        </v-list-item>
-      </v-card-actions>
-    </v-card>
+    <postCard :postsData="postsData" />
   </v-container>
 </template>
 <script>
+import postCard from "@/components/posts/postCard.vue";
 import storiesGroup from "@/components/stories/storiesGroup.vue";
 import { postsData } from "@/resources/postsDatabase";
 import { friendsData } from "@/resources/friendsDatabase";
@@ -97,7 +40,7 @@ export default {
         : "Good Afternoon";
     },
   },
-  components: { storiesGroup },
+  components: { storiesGroup, postCard },
   created() {
     postsData.map((post) => {
       let userObject = friendsData.filter(function (user) {

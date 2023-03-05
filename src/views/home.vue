266dcf08a1cf59fd7008d547837ec7ed1a416b26
VOIDRLM <>
2023-03-05 15:07:47 +0530

    Added validation for image url field

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,7 +8,12 @@
         >
         <storiesGroup />
         <postCard :postsData="postsData" />
-        <v-dialog transition="dialog-bottom-transition" max-width="800" content-class="rounded-xl">
+        <v-dialog
+            transition="dialog-bottom-transition"
+            max-width="800"
+            content-class="rounded-xl"
+            v-model="showNewPost"
+        >
             <template v-slot:activator="{ on, attrs }">
                 <v-fab-transition>
                     <v-btn
@@ -27,7 +32,7 @@
                     </v-btn>
                 </v-fab-transition>
             </template>
-            <template v-slot:default="dialog">
+            <template v-slot:default>
                 <v-card class="rounded-xl">
                     <v-toolbar color="accent" elevation="0" class="text-h6">Create a new post.</v-toolbar>
                     <v-form v-model="validPost">
@@ -39,25 +44,32 @@
                                 class="rounded-lg mt-5"
                                 counter="500"
                                 maxlength="500"
+                                v-model="postDescription"
                                 :rules="[(v) => !!v || '']"
                             ></v-textarea>
+                            <v-layout class="justify-center" v-if="imgLink !== '' && isValidUrl()">
+                                <v-img max-height="150" max-width="250" :src="imgLink" class="mb-3"></v-img>
+                            </v-layout>
                             <v-text-field
                                 label="Image Link"
                                 solo-inverted
                                 elevation="0"
                                 class="rounded-lg mt-2"
                                 flat
+                                v-model="imgLink"
+                                @input="isValidUrl"
+                                :rules="[(v) => isValidUrl() || v === '' || 'Invalid image URL']"
                             ></v-text-field>
                         </v-card-text>
                     </v-form>
                     <v-card-actions :class="!$vuetify.breakpoint.xsOnly ? 'justify-end' : 'justify-center'">
-                        <v-btn elevation="0" class="ma-3 pa-3" rounded @click="dialog.value = false">Close</v-btn>
+                        <v-btn elevation="0" class="ma-3 pa-3" rounded @click="showNewPost = false">Close</v-btn>
                         <v-btn
                             elevation="0"
                             class="ma-3 pa-3"
                             :disabled="!validPost"
                             rounded
-                            @click="dialog.value = false"
+                            @click="showNewPost = false"
                             >Post</v-btn
                         >
                     </v-card-actions>
@@ -73,7 +85,7 @@ import { postsData } from '@/resources/postsDatabase';
 import { friendsData } from '@/resources/friendsDatabase';
 export default {
     name: 'home-component',
-    data: () => ({ postsData, friendsData, validPost: true }),
+    data: () => ({ postsData, friendsData, showNewPost: false, validPost: true, postDescription: '', imgLink: '' }),
     computed: {
         getGreetingData() {
             var today = new Date();
@@ -95,13 +107,8 @@ export default {
         });
     },
     methods: {
-        isValidUrl(string) {
-            try {
-                new URL(string);
-                return true;
-            } catch (err) {
-                return false;
-            }
+        isValidUrl() {
+            return this.imgLink.match(/\.(jpeg|jpg|gif|png)$/) != null;
         },
     },
 };
